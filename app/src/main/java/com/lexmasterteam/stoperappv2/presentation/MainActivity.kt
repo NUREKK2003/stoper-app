68fc94d40882fcca61a38cf18ade013a9b827ac9
NUREKK2003 <jakubn20033002@gmail.com>
2022-06-28 23:57:40 +0200

    layout

---
+++ app/src/main/java/com/lexmasterteam/stoperappv2/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lexmasterteam.stoperappv2.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lexmasterteam.stoperappv2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
